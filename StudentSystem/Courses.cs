0185fad78f1d1040b26f203176df2aed9748e226
Antonia Dineva <antonia.d.neva@gmail.com>
2025-11-27 09:33:51 +0200

    courses

---
+++ StudentSystem/Courses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem
+{
+    public class Courses
+    {
+        [Key]
+        public int CourseId {  get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Students> AttendingStudents { get; set; }
+        public HomeworkSubmissions HomeworkSubmissions { get; set; }
+    }
+}
